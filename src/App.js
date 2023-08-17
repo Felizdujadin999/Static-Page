c5b7ca458d42cbbf1c3ca6fea1f237dabf482e4c
Felizdujadin <ezeikefelix51@gmail.com>
2023-08-17 16:02:08 +0100

    new codes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
-import Page from './component/staticPage';
+import SecondPage from './component/AffiliateMarketing';
+import FirstPage from './component/staticPage';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
 
 function App() {
   return (
-    <Page />
+    <Router>
+        <Routes>
+          <Route path='/' Component={FirstPage}/>
+          <Route path='/Sogbesan-Tantolorun/AffiiateMarketing' Component={SecondPage}/>
+        </Routes>
+    </Router>
     // <div className="App">
     //   <header className="App-header">
     //     <img src={logo} className="App-logo" alt="logo" />
